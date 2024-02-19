c3a862fcbd78c57ba44ec6175f0af22340a5d78d
Valentin Korneev <v.korneef@gmail.com>
2024-02-19 11:07:25 +0600

    Add functionality to preload all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import {useState, useEffect} from "react";
 import './App.css'
 import FlipCountdown from '@rumess/react-flip-countdown';
+import cn from "classnames";
+import image1 from './assets/images/1.jpg'
+import image2 from './assets/images/2.jpg'
+import image3 from './assets/images/3.jpg'
+import image4 from './assets/images/4.jpg'
+import image5 from './assets/images/5.jpg'
+import image6 from './assets/images/6.jpg'
 
 
 function App() {
   const [imageNumber, setImageNumber] = useState(1);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  console.log(imageNumber)
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -33,7 +38,13 @@ function App() {
 
   return (
     <div className="app">
-      <div key={imageNumber} className={`image-container bg-${imageNumber}`} />
+      <img className={cn(`image-container`, {['bg']: imageNumber === 1})} src={image1}/>
+      <img className={cn(`image-container`, {['bg']: imageNumber === 2})} src={image2}/>
+      <img className={cn(`image-container`, {['bg']: imageNumber === 3})} src={image3}/>
+      <img className={cn(`image-container`, {['bg']: imageNumber === 4})} src={image4}/>
+      <img className={cn(`image-container`, {['bg']: imageNumber === 5})} src={image5}/>
+      <img className={cn(`image-container`, {['bg']: imageNumber === 6})} src={image6}/>
+
       <div className={'time-wrapper'}>
         <div className='timer-description'>До начала просмотра "Атаки&nbsp;титанов" Адреем&nbsp;и&nbsp;Дашей</div>
         <FlipCountdown
