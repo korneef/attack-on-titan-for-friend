28d2fd014b005f87d8601d12e80db69b3767d5a3
Valentin Korneev <v.korneef@gmail.com>
2024-02-19 10:30:59 +0600

    add base to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/attack-on-titan-for-friend/",
   plugins: [react()],
 })
